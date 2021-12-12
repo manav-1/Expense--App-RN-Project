697e61d8bd33a4537b653fd68dfeb41bbe1fcfcb
manav-1 <manav81101@gmail.com>
2021-12-12 21:20:00 +0530

    Updated Icon and name

--- components/screens/Profile.js
+++ components/screens/Profile.js
@@ -101,40 +101,19 @@ const Profile = () => {
 
   return (
     <GradientContainer>
-      <ProgressBar
-        visible={isLoading}
-        progress={0.8}
-        color="#f1c0cb"
-        indeterminate
-      />
-      <View style={{ position: 'absolute', right: 10, top: 10 }}>
+      {isLoading ? (
+        <ProgressBar progress={0.8} color="#f1c0cb" indeterminate />
+      ) : null}
+      <View style={styles.editImageContainer}>
         <Image
           source={{
             uri:
               image ??
               'https://sitechecker.pro/wp-content/uploads/2017/12/URL-meaning.png'
           }}
-          style={{
-            width: 100,
-            height: 100,
-            borderRadius: 50,
-            backgroundColor: 'red'
-          }}
+          style={styles.profileImage}
         />
-        <TouchableOpacity
-          onPress={pickImage}
-          style={{
-            position: 'absolute',
-            right: -5,
-            bottom: -5,
-            backgroundColor: '#fff',
-            padding: 10,
-            paddingLeft: 12,
-            borderRadius: 50,
-            alignItems: 'center',
-            justifyContent: 'center'
-          }}
-        >
+        <TouchableOpacity onPress={pickImage} style={styles.editProfileButton}>
           <FontAwesome5 name="edit" size={18} color="#000" />
         </TouchableOpacity>
       </View>
@@ -149,7 +128,7 @@ const Profile = () => {
         <Text style={styles.text}>Email</Text>
         <ExpenseInput
           value={userEmail}
-          onChangeText={(val) => setUserEmail(val)}
+          editable={false}
           style={styles.expenseInput}
         />
 
@@ -160,20 +139,10 @@ const Profile = () => {
           </Text>
         </TouchableOpacity>
         <View style={{ alignSelf: 'flex-end' }}>
-          <Text
-            style={[
-              styles.text,
-              { fontSize: 16, textAlign: 'right', marginTop: 2 }
-            ]}
-          >
+          <Text style={[styles.text, styles.extraText]}>
             Account Created on
           </Text>
-          <Text
-            style={[
-              styles.text,
-              { fontSize: 16, textAlign: 'right', marginTop: 2 }
-            ]}
-          >
+          <Text style={[styles.text, styles.extraText]}>
             {user
               ? new Date(user.metadata.creationTime).toLocaleDateString()
               : null}
@@ -183,10 +152,10 @@ const Profile = () => {
       <Snackbar
         visible={snackbarVisible}
         duration={3000}
-        style={{ backgroundColor: '#f1c0cb', marginBottom: 80 }}
+        style={styles.snackbar}
         onDismiss={() => setSnackbarVisible(false)}
       >
-        <Text style={{ color: '#000' }}>{snackbarText}</Text>
+        <Text style={styles.snackbarText}>{snackbarText}</Text>
       </Snackbar>
     </GradientContainer>
   );
@@ -223,7 +192,28 @@ const styles = StyleSheet.create({
     color: '#000',
     fontFamily: 'karla',
     fontSize: 16
-  }
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: 'red'
+  },
+  editProfileButton: {
+    position: 'absolute',
+    right: -5,
+    bottom: -5,
+    backgroundColor: '#fff',
+    padding: 10,
+    paddingLeft: 12,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  extraText: { fontSize: 16, textAlign: 'right', marginTop: 2 },
+  snackbar: { backgroundColor: '#f1c0cb', marginBottom: 80 },
+  snackBarText: { color: '#000' },
+  editImageContainer: { position: 'absolute', right: 10, top: 10 }
 });
 
 export default Profile;
